0d0d5dd96e3d0ecb85b15af093b4fcc5154309ae
quantumVector <payned@mail.ru>
2021-11-10 07:10:43 +0300

    added strict mod

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
